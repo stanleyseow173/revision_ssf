4e23e3b5a40e61863beb433d3f75873f25337a51
stanleyseow173 <seowszehowestanley@gmail.com>
2023-03-15 14:31:48 +0800

    first commit

---
+++ src/main/java/sg/nus/edu/iss/revision/RevisionApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.edu.iss.revision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisionApplication.class, args);
+	}
+
+}
---
+++ src/main/java/sg/nus/edu/iss/revision/config/RedisConfig.java
@@ -0,0 +1,70 @@
+package sg.nus.edu.iss.revision.config;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+ 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+    
+
+    @Value("${spring.data.redis.port}")
+    private Optional<Integer> redisPort;
+    
+
+    @Value("${spring.data.redis.user}")
+    private String redisUsername;
+    
+
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
+    
+    @Bean("pizza")
+    @Scope("singleton")
+    public RedisTemplate<String, String> redisTemplate(){
+        final RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisHost);
+        config.setPort(redisPort.get());
+
+        if(!redisUsername.isEmpty() && !redisPassword.isEmpty()){
+            config.setUsername(redisUsername);
+            config.setPassword(redisPassword);
+        }
+        config.setDatabase(0);
+        final JedisClientConfiguration jedisClient =  JedisClientConfiguration
+                                .builder()
+                                .build();
+
+        final JedisConnectionFactory jedisFac = new JedisConnectionFactory(config, 
+                jedisClient);
+        jedisFac.afterPropertiesSet();
+        
+        RedisTemplate<String, String> r = new RedisTemplate<String,String>();
+        r.setConnectionFactory(jedisFac);
+        r.setKeySerializer(new StringRedisSerializer());
+        // r.setHashKeySerializer(new StringRedisSerializer());
+
+        // RedisSerializer<Object> objSerializer 
+        //          = new JdkSerializationRedisSerializer(getClass().getClassLoader());
+                
+        //r.setValueSerializer(objSerializer);
+        r.setValueSerializer(new StringRedisSerializer());
+        // r.setHashValueSerializer(objSerializer);
+        
+        System.out.println("redisHost > " + redisHost);
+        return r;
+    }
+}
